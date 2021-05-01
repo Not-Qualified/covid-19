4230e25f7453e8e6dc68b15d774d438e9d0db5c3
virenparmar12 <virenparmar1912@gmail.com>
2021-05-01 11:40:14 +0530

    Update - Setting.py Database Settings Updated

--- cvd/settings.py
+++ cvd/settings.py
@@ -84,22 +84,22 @@ WSGI_APPLICATION = 'cvd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'OPTIONS': {
-#             'read_default_file': '/opt/db.cnf',
-#             'init_command': 'SET default_storage_engine=INNODB',
-#         },
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/opt/db.cnf',
+            'init_command': 'SET default_storage_engine=INNODB',
+        },
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
